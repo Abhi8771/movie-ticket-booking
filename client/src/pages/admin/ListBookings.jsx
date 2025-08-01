e0a064e3fa23e8c386379aa005c6ddb02182564f
Abhi8771 <abhi8771@gmail.com>
2025-08-01 12:08:29 +0530

    title-fixed

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Loading from '../../components/Loading';
-import Title from "../../components/admin/Title";
+import Title from "../../components/admin/title";
 import dateFormat from '../../lib/dateFormat';
 import { useAppContext } from '../../context/AppContext';
 
