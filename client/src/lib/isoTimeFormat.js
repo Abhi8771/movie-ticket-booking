b4fee3da6ae30e4bbdbf69534e22b262cca606ac
Abhi8771 <abhi8771@gmail.com>
2025-08-21 09:46:19 +0530

    timezone-fix

--- client/src/lib/isoTimeFormat.js
+++ client/src/lib/isoTimeFormat.js
@@ -10,14 +10,33 @@
 
 // // export default isoTimeFormat ;
 
-const isoTimeFormat = (timeStr) => {
+// const isoTimeFormat = (timeStr) => {
+//   const [h, m] = timeStr.split(':');
+//   let hour = parseInt(h, 10);
+//   const minute = m.padStart(2, '0');
+//   const ampm = hour >= 12 ? 'PM' : 'AM';
+//   if (hour > 12) hour -= 12;
+//   if (hour === 0) hour = 12;
+//   return `${hour}:${minute} ${ampm}`;
+// };
+
+// export default isoTimeFormat;
+
+const isoTimeFormatIST = (timeStr) => {
   const [h, m] = timeStr.split(':');
-  let hour = parseInt(h, 10);
-  const minute = m.padStart(2, '0');
-  const ampm = hour >= 12 ? 'PM' : 'AM';
-  if (hour > 12) hour -= 12;
-  if (hour === 0) hour = 12;
-  return `${hour}:${minute} ${ampm}`;
+  const date = new Date();
+
+  // Treat input as UTC time
+  date.setUTCHours(parseInt(h, 10), parseInt(m, 10), 0, 0);
+
+  // Convert to IST explicitly
+  return date.toLocaleTimeString('en-IN', {
+    timeZone: 'Asia/Kolkata',  // IST (UTC+5:30)
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  });
 };
 
-export default isoTimeFormat;
+export default isoTimeFormatIST;
+
