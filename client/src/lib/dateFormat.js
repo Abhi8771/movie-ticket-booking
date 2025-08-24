1d9d83682361c3b5adf3ca883a1802ceeaa01ed1
Abhi8771 <abhi8771@gmail.com>
2025-08-24 15:28:15 +0530

    date -fix

--- client/src/lib/dateFormat.js
+++ client/src/lib/dateFormat.js
@@ -1,11 +1,25 @@
-export const dateFormat = (date) => {
-    return new Date(date).toLocaleDateString('en-US',{
-        weekday: 'short',
-        month:'long',
-        day:'numeric',
-        hour:'numeric',
-        minute:'numeric'
-    })
-}
+// export const dateFormat = (date) => {
+//     return new Date(date).toLocaleDateString('en-US',{
+//         weekday: 'short',
+//         month:'long',
+//         day:'numeric',
+//         hour:'numeric',
+//         minute:'numeric'
+//     })
+// }
 
-export default dateFormat
+// export default dateFormat
+
+export const dateFormat = (date) => {
+    return new Date(date).toLocaleDateString('en-US', {
+        weekday: 'short',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+        timeZone: 'Asia/Kolkata'
+    });
+};
+
+export default dateFormat;
